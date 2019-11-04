4b5a37773ab675e552246c9ab715396f3200f096
Angela Pham <angpham@chapman.edu>
2019-11-04 11:22:21 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 # username.sh
-# author
+# Angela Pham
 echo "The only characters that can be used are lowercase letters, digits, and the underscore character."
 echo "It must start with a loweracse letter"
 echo "It must contain at least 3 characters, but no more than 12 characters."
 echo "Enter a valid username: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
-do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
+do 
+	echo "Sorry, this is not a valid username."
+	echo "Enter a valid username: "
+	read USERNAME
 done
 echo "Thank you"
