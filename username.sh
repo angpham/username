62f87d72574d42df9ea455f9bd6a242602f51ca2
Angela Pham <angpham@chapman.edu>
2019-11-10 17:15:26 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -6,7 +6,7 @@ echo "It must start with a loweracse letter"
 echo "It must contain at least 3 characters, but no more than 12 characters."
 echo "Enter a valid username: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do 
 	echo "Sorry, this is not a valid username."
 	echo "Enter a valid username: "
